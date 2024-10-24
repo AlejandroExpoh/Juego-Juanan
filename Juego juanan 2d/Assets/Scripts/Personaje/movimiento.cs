156aabb8eb27739830e3244214d64228abb19462
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-10-24 13:29:14 +0200

    Movimiento 2

--- Juego juanan 2d/Assets/Scripts/Personaje/movimiento.cs
+++ Juego juanan 2d/Assets/Scripts/Personaje/movimiento.cs
@@ -6,6 +6,7 @@ public class movimiento : MonoBehaviour
 {
     public float speed = 3f;
     public Rigidbody2D personajeRB;
+    public float rota = 5f;
     float moveX;
     float moveY;
     // Start is called before the first frame update
@@ -15,18 +16,31 @@ public class movimiento : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        movement();
+        rotation();
+        
+
+        /*Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;*/
+    }
+
+    public void movement()
     {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
         Vector2 fullMovement = new Vector2(moveX, moveY).normalized * speed * Time.deltaTime;
         personajeRB.velocity = fullMovement;
 
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-
-        transform.up = direction;
+    }
+    public void rotation()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rota * Time.deltaTime);
     }
 }
