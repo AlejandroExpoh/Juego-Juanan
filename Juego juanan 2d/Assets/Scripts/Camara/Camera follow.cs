98099de10201dea570fd48463c1b1e5de98e7382
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-11-06 14:32:10 +0100

    camera 2.0 2

--- Juego juanan 2d/Assets/Scripts/Camara/Camera follow.cs
+++ Juego juanan 2d/Assets/Scripts/Camara/Camera follow.cs
@@ -16,7 +16,27 @@ public void Setup(Func<Vector3> GetcameraFollowPositionFunc)
     {
         Vector3 cameraFollowPosition = GetcameraFollowPositionFunc();
         cameraFollowPosition.z = transform.position.z;
-        transform.position = cameraFollowPosition;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+        float cameraMoveSpeed = 3f;
+
+
+        if (distance > 0)
+        {
+            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
+
+            if (distanceAfterMoving > distance)
+            {
+                newCameraPosition = cameraFollowPosition;
+            } 
+
+            transform.position = newCameraPosition;
+
+        }
+        
         
     }
 }
