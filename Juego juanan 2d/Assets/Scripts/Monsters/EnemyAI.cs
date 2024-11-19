0b652b5bbd67ffe848b8fc61242c5b202a38f443
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-11-19 11:07:03 +0100

    a*

--- Juego juanan 2d/Assets/Scripts/Monsters/EnemyAI.cs
+++ Juego juanan 2d/Assets/Scripts/Monsters/EnemyAI.cs
@@ -15,6 +15,7 @@ public class EnemyAI : MonoBehaviour
     Path path;
     int currentWaypoint = 0;
     bool reachedEndOfPath = false;
+   
 
     Seeker seeker;
     Rigidbody2D rb;
@@ -46,6 +47,11 @@ public class EnemyAI : MonoBehaviour
   
     void FixedUpdate()
     {
+
+       
+
+
+
         if (path == null)
             return;
 
@@ -57,12 +63,12 @@ public class EnemyAI : MonoBehaviour
         {
             reachedEndOfPath=false;
         }
+        
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            Vector2 force = direction * speed * Time.deltaTime;
 
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
-
-        rb.AddForce(force);
-
+            rb.AddForce(force);
+        
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
         if (distance < nextWaypointDistance)
