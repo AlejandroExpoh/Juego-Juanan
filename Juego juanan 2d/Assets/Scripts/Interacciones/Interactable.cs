9ff188b6201b824789083523d74c16676f64349c
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-11-20 14:17:53 +0100

    Puzzles

---
+++ Juego juanan 2d/Assets/Scripts/Interacciones/Interactable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode InteractKey;
+    public UnityEvent interactAction;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(InteractKey))
+            {
+                interactAction.Invoke();
+
+            }
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player esta en posición");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInRange = false;
+        Debug.Log("Player no esta en posición");
+    }
+
+
+}
