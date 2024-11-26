779df4cb4f4e4bf677e4db6729b06736cd6bebbb
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-11-26 11:40:55 +0100

    monster

--- Juego juanan 2d/Assets/Scripts/Monsters/Chasing.cs
+++ Juego juanan 2d/Assets/Scripts/Monsters/Chasing.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Chasing : MonoBehaviour
 {
 
-    public bool AwareOfPlayer {  get; private set; }
-    public Vector2 DirectionToPlayer {  get; private set; }
+    public bool AwareOfPlayer { get; private set; }
+    public Vector2 DirectionToPlayer { get; private set; }
 
     [SerializeField]
     private float _playerAwarenessDistance;
@@ -32,32 +32,37 @@ public class Chasing : MonoBehaviour
         aiDestinationSetter = GetComponent<AIDestinationSetter>();
         player = GameObject.FindWithTag("Player").transform;
         point = GameObject.FindWithTag("Point").transform;
-    } 
+    }
 
-    
+
     void Update()
     {
+        RaycastHit2D vision = Physics2D.Raycast(transform.position, -transform.position+player.position, Mathf.Infinity);
         Vector2 enemyToPlayerVector = player.position - transform.position;
         DirectionToPlayer = enemyToPlayerVector.normalized;
-
-        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance)
+        Debug.Log(vision.collider.gameObject.tag);
+        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance && vision.collider.gameObject.CompareTag("Player"))
         {
-            Aware = true;
+           
+            Aware = true;  
         }
         else
         {
-            Aware = false;
+            //Aware = false;
         }
 
         if (Aware)
         {
             aiDestinationSetter.target = player;
         }
-        else 
+        else
         {
             aiDestinationSetter.target = point;
         }
-
     }
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, -transform.position + player.position);
+    }
 }
