f20c14248d8bd952e067bcd895c585c3097af31e
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-12-02 13:15:37 +0100

    Camara y aura escondidos

--- Juego juanan 2d/Assets/Scripts/Interacciones/Hiding.cs
+++ Juego juanan 2d/Assets/Scripts/Interacciones/Hiding.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Hiding : MonoBehaviour
 {
     Chasing Chasing;
     public GameObject Enemigo;
     public bool escondido = false;
+    [Header("Camara")]
     public Camera CamPrincipal;
-    public float normal, cerca,tiempo;
+    public float tiempo, cerca;
+    float actual, normal;
+
+    [Header("Luz")]
+    public Transform aura;
+    public float tamanyo;
+    Vector3 descubierto, actualuz, cubierto;
 
     void Start()
     {
        Chasing = Enemigo.GetComponent<Chasing>();
+        normal = CamPrincipal.orthographicSize;
+        actual = normal;
+        descubierto = aura.transform.localScale;
+        actualuz = descubierto;
+        cubierto = descubierto * tamanyo;
+        
     }
 
    
@@ -22,19 +36,25 @@ public class Hiding : MonoBehaviour
         if (escondido)
         {
             Chasing._playerAwarenessDistance = 0;
-            CamPrincipal.GetComponent<Camera>().orthographicSize = cerca;
+            actual = cerca;
+            actualuz = cubierto;
+            
         }
         else
         {
             Chasing._playerAwarenessDistance = 13;
-            CamPrincipal.GetComponent<Camera>().orthographicSize = normal;
+            actual = normal;
+            actualuz = descubierto;
         }
+        CamPrincipal.orthographicSize = Mathf.Lerp(CamPrincipal.orthographicSize, actual, tiempo * Time.deltaTime);
+        aura.transform.localScale = Vector3.Lerp(aura.transform.localScale, actualuz, tiempo * Time.deltaTime);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             escondido = true;
+            
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
